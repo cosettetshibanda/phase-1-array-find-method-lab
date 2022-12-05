a25ac14fa970b6d7fcf196b4e80de67d8345fbbf
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-12-05 13:31:40 -0700

    commit tree b8124ef351b01897138d8834822addf470b4c488

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+// function superbowlWin(year, finder){
+//     let result = undefined
+//     for(let item of result){
+//         if(finder(item) === true){
+//             result = year
+//         }
+//     }
+// }
+function superbowlWin(record){
+    let found = record.find(object => object.result === 'W' )
+    console.log(found)
+    // return found.year
+    if(found === undefined){
+        return undefined
+    }
+    else{
+        return found.year
+    }
+
+    // if(year.result === 'W'){
+    //     return year
+    // }
+    // else{
+    //     return undefined
+    // }
+    }
+    
+// let superbowlWin = record.find((record) => {return record.year === "W"})
